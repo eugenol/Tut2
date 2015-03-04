4881d2e751b74f48578f20fc5a0d1d3fb5e1c1d4
eugenol <eug@live.co.za>
2015-03-04 17:32:50 +0200

    add method to read mixed fractions

--- Tut2/Tut2.cpp
+++ Tut2/Tut2.cpp
@@ -4,32 +4,22 @@
 #include <iostream>
 #include <string>
 #include "Fraction.h"
+#include <cstdio> //Is this needed?
 
 using namespace std;
 
-void print(Fraction);
-Fraction read(void);
+Fraction readfraction(void);
 
 int main(int argc, char* argv[])
 {
 	Fraction frac1, frac2, res;
 	int temp;
 
-	cout << "Enter the numerator for fraction 1: ";
-	cin >> temp;
-	frac1.setNumerator(temp);
+	cout << "Enter the fraction 1: ";
+	frac1 = readfraction();
 
-	cout << endl << "Enter the denominator for fraction 1: ";
-	cin >> temp;
-	frac1.setDenominator(temp);
-
-	cout << endl << "Enter the numerator for fraction 2: ";
-	cin >> temp;
-	frac2.setNumerator(temp);
-
-	cout << endl << "Enter the denominator for fraction 2: ";
-	cin >> temp;
-	frac2.setDenominator(temp);
+	cout << "Enter the fraction 2: ";
+	frac2 = readfraction();
 
 	//With operator overloading
 
@@ -50,24 +40,53 @@ int main(int argc, char* argv[])
 	res.print();
 	cout << endl;
 
-	cout << "Enter mixed fraction";
-	res = read();
-
 	return 0;
 }
 
-Fraction read(void)
+Fraction readfraction(void)
 {
 	Fraction b;
 	string temp;
-//	int whole;
+	int whole = 0, num = 0, den = 1;
 
 	getline(cin,temp);
 
 	if (temp.find(" ") != string::npos)
 	{
-		string temp2 = temp.substr(0, temp.find(" ") - 1);
-		cout << temp2;
+		int pos = temp.find(" ");
+		string temp2 = temp.substr(0, pos);
+		whole = atoi(temp2.c_str());
+
+		if (temp.find("/", pos + 1) != string::npos)
+		{
+			int pos2 = temp.find("/", pos + 1);
+			temp2 = temp.substr(pos + 1, pos2);
+			num = atoi(temp2.c_str());
+			temp2 = temp.substr(pos2 + 1);
+			den = atoi(temp2.c_str());
+
+			b.setNumerator(whole*den + num);
+			b.setDenominator(den);
+
+		}
+		else
+			b.setNumerator(whole);
+	}
+	else if (temp.find("/") != string::npos)
+	{
+			int pos = temp.find("/");
+			string temp2 = temp.substr(0, pos);
+			num = atoi(temp2.c_str());
+			temp2 = temp.substr(pos + 1);
+			den = atoi(temp2.c_str());
+
+			b.setNumerator(num);
+			b.setDenominator(den);
+	}
+	else
+	{
+		num = atoi(temp.c_str());
+		b.setNumerator(num);
 	}
 
 	return b;
